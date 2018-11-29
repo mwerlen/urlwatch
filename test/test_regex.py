32d753f014754899cc787359189ef51a40da92cd
mwerlen <maxime@werlen.fr>
2018-11-29 21:43:24 +0100

    new regex

--- test/test_regex.py
+++ test/test_regex.py
@@ -5,17 +5,23 @@ import unittest
 WDIFF_ADDED_RE_I = r'[{][+].*?[+][}]'
 WDIFF_REMOVED_RE_I = r'[\[][-].*?[-][]]'
 
-WDIFF_ADDED_RE_M = r'[{][+].*?[+][}]'
-WDIFF_REMOVED_RE_M = r'[\[][-].*?[-][]]'
+WDIFF_ADDED_RE_M = r'{\+.*?\+}'
+WDIFF_REMOVED_RE_M = r'\[-.*?-\]'
 
 
 class TestRegex(unittest.TestCase):
     def test_wdiff_regex(self):
-        line = u"This is a [-bad-] {+good+} test"
-        groups1 = re.split(WDIFF_ADDED_RE_I, line)
-        groups2 = re.split(WDIFF_ADDED_RE_M, line)
+        line = u"""
+        urlwatch [-is-] {+was+} intended to help you watch changes in [-webpages-] {+webpages, files, folders...+} and get notified (via e-mail, in your terminal or through various third party services) of any changes.
+        The change notification will include the [-URL-] {+URL/file/folder+} that has changed and a unified diff of what has changed.
+        {+There is a lot of reporters+}
+        """
+        groups1 = re.findall(WDIFF_ADDED_RE_I, line)
+        groups2 = re.findall(WDIFF_ADDED_RE_M, line)
         self.assertListEqual(groups1, groups2)
+        self.assertEqual(len(groups1), 4)
 
-        groups1 = re.split(WDIFF_REMOVED_RE_I, line)
-        groups2 = re.split(WDIFF_REMOVED_RE_M, line)
+        groups1 = re.findall(WDIFF_REMOVED_RE_I, line)
+        groups2 = re.findall(WDIFF_REMOVED_RE_M, line)
         self.assertListEqual(groups1, groups2)
+        self.assertEqual(len(groups1), 3)
