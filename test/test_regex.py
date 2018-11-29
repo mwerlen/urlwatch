7e4a5e0511d206d708ba239ce8137d7a4a579667
mwerlen <maxime@werlen.fr>
2018-11-29 21:33:32 +0100

    PyCodeStyle is Ok

--- test/test_regex.py
+++ test/test_regex.py
@@ -8,14 +8,14 @@ WDIFF_REMOVED_RE_I = r'[\[][-].*?[-][]]'
 WDIFF_ADDED_RE_M = r'[{][+].*?[+][}]'
 WDIFF_REMOVED_RE_M = r'[\[][-].*?[-][]]'
 
+
 class TestRegex(unittest.TestCase):
     def test_wdiff_regex(self):
         line = u"This is a [-bad-] {+good+} test"
         groups1 = re.split(WDIFF_ADDED_RE_I, line)
         groups2 = re.split(WDIFF_ADDED_RE_M, line)
-        self.assertListEqual(groups1,groups2)
-    
+        self.assertListEqual(groups1, groups2)
+
         groups1 = re.split(WDIFF_REMOVED_RE_I, line)
         groups2 = re.split(WDIFF_REMOVED_RE_M, line)
-        self.assertListEqual(groups1,groups2)
-
+        self.assertListEqual(groups1, groups2)
